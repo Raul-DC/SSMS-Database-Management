7dcedc5f46a1cb1e50901c4fbbb70c17a1b4f7f0
Raul Diaz <93233978+Raul-DC@users.noreply.github.com>
2024-11-04 07:18:58 -0300

    Update remove_duplicates.sql

--- scripts/remove_duplicates.sql
+++ scripts/remove_duplicates.sql
@@ -1,5 +1,7 @@
+---------------------------------------------------------------------------------------------------------------------------
+
 -- remove_duplicates.sql
--- Script to remove duplicate records from the dbo.Unificado table
+-- Script to manually remove duplicate records from the dbo.Unificado table
 
 WITH CTE_Duplicates AS (
     SELECT 
@@ -15,5 +17,52 @@ WITH CTE_Duplicates AS (
 DELETE FROM CTE_Duplicates 
 WHERE rn > 1;
 
+---------------------------------------------------------------------------------------------------------------------------
+
 -- Optional: To verify which records will be deleted, you can first run this query:
 -- SELECT * FROM CTE_Duplicates WHERE rn > 1;
+
+---------------------------------------------------------------------------------------------------------------------------
+
+-- Script to create de ExecutionLogs table:
+
+CREATE TABLE dbo.ExecutionLogs (
+    ExecutionDate DATETIME,
+    AffectedRows INT,
+    Instance NVARCHAR(100),
+    Message NVARCHAR(255)
+);
+
+---------------------------------------------------------------------------------------------------------------------------
+
+-- Script used to eliminate duplicates and store logs:
+
+BEGIN TRY
+    -- Variables to store log information
+    DECLARE @AffectedRows INT;
+    DECLARE @Instance NVARCHAR(100) = @@SERVERNAME;
+    
+    -- Remove duplicates
+    WITH CTE_Duplicates AS (
+        SELECT 
+            CHROM, POS, ID, REF, ALT, QUAL, FILTER, INFO, FORMAT, MUESTRA, VALOR, ORIGEN, FECHA_COPIA,
+            ROW_NUMBER() OVER (PARTITION BY ID, MUESTRA, RESULTADO ORDER BY FECHA_COPIA DESC) AS rn
+        FROM dbo.Unificado
+    )
+    DELETE FROM CTE_Duplicates WHERE rn > 1;
+
+    -- Save the number of affected rows
+    SET @AffectedRows = @@ROWCOUNT;
+
+    -- Insert into the logs table
+    INSERT INTO dbo.ExecutionLogs (ExecutionDate, AffectedRows, Instance, Message)
+    VALUES (GETDATE(), @AffectedRows, @Instance, 'Duplicate removal process executed');
+
+END TRY
+BEGIN CATCH
+    -- In case of error, a log of the error can be saved
+    INSERT INTO dbo.ExecutionLogs (ExecutionDate, AffectedRows, Instance, Message)
+    VALUES (GETDATE(), 0, @@SERVERNAME, ERROR_MESSAGE());
+END CATCH;
+
+---------------------------------------------------------------------------------------------------------------------------
